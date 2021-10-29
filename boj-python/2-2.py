ec46d12b1fbae47fcc12220c5c4dca73eed9f642
seunghoon-shin <liante0904@gmail.com>
2021-10-29 19:50:51 +0900

    Restart Commit..

---
+++ boj-python/2-2.py
@@ -0,0 +1,18 @@
+'''
+문제
+시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+출력
+시험 성적을 출력한다.
+'''
+
+nInput = int(input())
+
+if nInput >= 90 and nInput <= 100: print('A')
+elif nInput >= 80 and nInput <= 89: print('B')
+elif nInput >= 70 and nInput <= 79: print('C')
+elif nInput >= 60 and nInput <= 69: print('D')
+else: print('F')
